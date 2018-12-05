f728f5a85bc9eceb7c3f4c1fc8962e05d87dbeb4
Kendrick Ang <phantom2457@hotmail.com>
2018-12-06 00:04:58 +0800

    added axios

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var body_parser = require('body-parser');
+var axios = require('axios');
+const api = "https://api.telegram.org/bot671406474:AAE_PSiJw8aESyuXN58-IRR9kH97v3SVcFE/";
 const port = 3000;
 
 app.use(body_parser.json()); // set application/json parser as middleware
@@ -8,13 +10,24 @@ app.use(body_parser.urlencoded({
     extended: true
 })); // set application/x-www-form-urlencoded as middleware
 
-// POST sends data to a server to create/update a resource
-app.post('/new-message', function(req, res) {
-    const message = req.body;
-    console.log(message);
-});
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    axios.get(api + "getMe")
+        .then(function(response) {
+            console.log(response.data.result);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    res.send("done");
 });
+
+app.get('/sendMessage', function(req, res) {
+    const input = {
+        chat_id = "StillAliveBot",
+        text = "hello"
+    };
+    axios.post(api + "sendMessage", input)
+        .then()
+})
 
 app.listen(port, () => console.log("Listening on port 3000!"));
