150a1460cdd351e0ea0c09c1a7c02b909dd0f8a9
Kendrick Ang <phantom2457@hotmail.com>
2018-12-05 11:56:51 +0800

    Minor changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # StillAliveBot
 Telegram bot
+
+THINGS NEEDED:
+1. HTTP client to communicate with server (what will this server be?)
+2. An application server
+3. Something to parse the requests from phone 
+
+axios, express, body-parser
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
+var body_parser = require('body-parser');
 const port = 3000;
 
+app.use(body_parser.json()); // set application/json parser as middleware
+app.use(body_parser.urlencoded({
+    extended: true
+})); // set application/x-www-form-urlencoded as middleware
+
+// POST sends data to a server to create/update a resource
+app.post('/new-message', function(req, res) {
+    const message = req.body;
+    console.log(message);
+});
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
